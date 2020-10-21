48877101fb7d2e3cc7d432144e2dd2aecfc29bc2
Fernando <35590251+FernandoGuardado@users.noreply.github.com>
2020-10-21 10:19:54 -0600

    .

---
+++ build/precache-manifest.5a3fa75fc37e1b63c2b569b4ba45b53d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f61c847a25f9a8a8dd418e80865e44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4546e87534f816f085dd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5dc21fb7fe6b97c2281e",
+    "url": "/static/js/2.338f4023.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.338f4023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4546e87534f816f085dd",
+    "url": "/static/js/main.e43ef1a1.chunk.js"
+  },
+  {
+    "revision": "41d9abee499da010ed98",
+    "url": "/static/js/runtime-main.86369114.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
