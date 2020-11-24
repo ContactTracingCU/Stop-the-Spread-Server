182c79518d19d87116eb02f215b9162fdd6251ba
Fernando <35590251+FernandoGuardado@users.noreply.github.com>
2020-11-24 03:24:39 -0700

    Worker tests

--- src/contactTracing/handlerTest.js
+++ src/contactTracing/handlerTest.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use(
-    express.urlencoded({
-        extended = true
-    })
-)
-app.use(express.json());
+// app.use(
+//     express.urlencoded({
+//         extended = true
+//     })
+// )
+// app.use(express.json());
 console.log(app);
 // const spawn = require('child_process').spawn;
 // const process = spawn('python', ['./firebaseTest.py']);
