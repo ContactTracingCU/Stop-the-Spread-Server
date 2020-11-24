ae624ca2e07e890f01de46ccc528171d468a586e
Fernando <35590251+FernandoGuardado@users.noreply.github.com>
2020-11-24 02:51:50 -0700

    Worker file tests

---
+++ src/contactTracing/handlerTest.js
@@ -0,0 +1,7 @@
+// const spawn = require('child_process').spawn;
+// const process = spawn('python', ['./firebaseTest.py']);
+
+// process.stdout.on('data', data => {
+//     console.log(data.toString());
+// })
+setInterval(console.log('worker file is working'), 10000);
