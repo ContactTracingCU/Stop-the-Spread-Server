51f042661e0effdacc0488265cbb58726fa40d5b
Fernando <35590251+FernandoGuardado@users.noreply.github.com>
2020-11-25 05:37:28 -0700

    Added authProvider.py Removed firebase operations from contactTracing.py and made it it's own file AuthProvider.py will now handle all firebase operations

--- src/contactTracing/contactTracing.py
+++ src/contactTracing/contactTracing.py
@@ -1,45 +1,15 @@
 import pyrebase
 import time
+import authProvider
 # import coordinateMath
 
 # TODO 
 # add while loop to check every x seconds after deletion of queue so
 # we don't calculate on same information twice
 
-print('Contact tracing worker file up')
+print('Contact tracing worker file running...')
 
-apiKey = "AIzaSyCl2hHT9n6qPGyoqTOXsRqi_3QJcETx2YA"
-authDomain = "sts0-76694.firebaseapp.com"
-databaseURL = "https://sts0-76694.firebaseio.com/"
-storageBucket = "sts0-76694.appspot.com"
-
-config = {
-  "apiKey": apiKey,
-  "authDomain": authDomain,
-  "databaseURL": databaseURL,
-  "storageBucket": storageBucket
-}
-
-# get a reference to the firebase, auth service, database service and storage service
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-db = firebase.database()
-storage = firebase.storage()
-
-# get dictionary of locations, users and testedPositive users
-locations = db.child('locations').get().val()
-users = db.child('users').get().val()
-testedPositive = db.child('testedPositive').get().val()
-
-print(
-'''
-Initialized necessary firebase components
-apiKey : {}
-authDomain : {}
-databaseURL : {}
-storageBucket : {}
-'''.format(apiKey, authDomain, databaseURL, storageBucket)
-)
+locations, users, testedPositive = authProvider.setUpFirebase()
 
 # BEGINING OF CONTACT TRACING ALGORITHM
   # TODO 
