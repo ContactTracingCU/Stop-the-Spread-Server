478f88e9ad1023ffa19997382df524282cf4cea9
Fernando <35590251+FernandoGuardado@users.noreply.github.com>
2020-11-24 23:36:19 -0700

    Handler test file on heroku server

--- handlerTest.js
+++ handlerTest.js
@@ -1,12 +1,10 @@
-console.log('1');
 const spawn = require('child_process').spawn;
-const process = spawn('python', ['./firebaseTest.py']);
-console.log('2');
+const process = spawn('python', ['firebaseTest.py']);
+
 process.stdout.on('data', data => {
     console.log(data.toString());
     console.log('worker file runs firebaseTest.py');
 })
-console.log('3');
 
 // setInterval(testConsole, 10000);
 
@@ -14,4 +12,6 @@ console.log('3');
 //     console.log('worker file is working');
 // }
 // console.log('worker file is working');
+
+
 
